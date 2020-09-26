783d73c6858e983586d95dac966abd52e4e53254
josephtmartin <goldglovejo13@gmail.com>
2020-09-25 19:59:43 -0500

    added firebase auth through google and created the navbar with site name and logout button

---
+++ src/javascripts/components/navbar.js
@@ -0,0 +1,42 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const logoutEvent = () => {
+  $('#navbar-logout-button').on('click', (e) => {
+    e.preventDefault();
+    window.sessionStorage.removeItem('ua');
+    firebase.auth().signOut();
+    window.location.href = '/';
+  });
+};
+
+const myNavbar = () => {
+  $('#nav').html(
+    `    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Almost Pinterest</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <button class="nav-link btn btn-danger" id="navbar-logout-button">Logout</button>
+        </li>
+      </ul>
+    </div>
+  </nav>`
+  );
+
+  logoutEvent();
+};
+
+export default { myNavbar };
